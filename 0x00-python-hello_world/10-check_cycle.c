9acd4a2cb32ec7da17438d02a08712055e1213d4
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-04-25 16:19:53 -0700

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* check_cycle - function that checks if the cycle of the 
+* linked list exist or not
+*
+* @list: list to check
+*
+* Return: 0 if there is no a cycle, 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *nextnextnode, *nextnode;
+	nextnode = list;
+	nextnextnode = list;
+
+	while (list != NULL)
+	{
+		nextnode = nextnode->next;
+		nextnextnode = nextnextnode->next->next;
+	
+		if (nextnode == nextnextnode)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
