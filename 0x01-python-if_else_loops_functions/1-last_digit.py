01e570638c08f9bb8234784fbbb34abbce5499f7
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-04-26 10:38:36 -0700

    all works ok but the checken dont run

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,5 @@ if lstdigit > 5:
     print(f"Last digit of {number} is {lstdigit} and is greater than 5")
 elif lstdigit == 0:
     print(f"Last digit of {number} is {lstdigit} and is 0")
-else
-    print(f"Last digit of {number} is {lstdigit} and is less than 6 and not 0)
+else:
+    print(f"Last digit of {number} is {lstdigit} and is less than 6 and not 0")
