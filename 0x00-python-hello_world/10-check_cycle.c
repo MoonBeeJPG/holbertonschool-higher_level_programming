c04f4c02765f72a154a42293f7eab8c4e92a9432
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-04-25 17:20:22 -0700

    a little error in output again

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,14 +12,12 @@
 int check_cycle(listint_t *list)
 {
 listint_t *nextnextnode = list, *nextnode = list;
-
 	while (nextnextnode->next != NULL && nextnextnode->next->next != NULL && list != NULL)
 	{
 		nextnode = nextnode->next;
 		nextnextnode = nextnextnode->next->next;
-
 		if (nextnode == nextnextnode)
 			return (1);
 	}
-	return (0);
+	return(0);
 }
