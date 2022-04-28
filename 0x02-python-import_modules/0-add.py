74d5052522a67afd2c1f2111d4450868f17aad54
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-04-28 07:42:45 -0700

    import check

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import add_0 as add
-a = 1
-b = 2
-add = add.add(a, b)
-print(f"{a} + {b} = {add}")
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    add = add.add(a, b)
+    print(f"{a} + {b} = {add}")
