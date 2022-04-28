08adc970231a56b0fca6a0658a680f8d3ef46863
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-04-28 14:04:26 -0700

    infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    arguments = len(sys.argv) - 1
-    i = 0
-    position = 1
+    import sys
+    arguments = len(sys.argv)
+    addition = 0
 
-    if i in range(int(sys.argv)):
-        sum += int(i)
-        position += 1
-
-    print(f"{sum}")
+    for i in range(1, arguments):
+        addition += int(sys.argv[i])
+    print(f"{addition}")
