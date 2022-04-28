17453276377efb79e3db59452446c71cb981445d
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-04-28 08:08:16 -0700

    task one check

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
-import calculator_1 as add
-import calculator_1 as sub
-import calculator_1 as mul
-import calculator_1 as div
+from calculator_1 import add, sub, mul,div
 
 if __name__ == "__main__":
     a = 10
     b = 5
 
-    addition = f"{a} + {b}"
-    substraction = f"{a} - {b}"
-    multiplication = f"{a} * {b}"
-    division = f"{a} / {b}"
-
-    addresult = add.add(a, b)
-    subresult = sub.sub(a, b)
-    mulresult = mul.mul(a, b)
-    divresult = div.div(a, b)
-
-    print(f"{addition} = {addresult}")
-    print(f"{substraction} = {subresult}")
-    print(f"{multiplication} = {mulresult}")
-    print(f"{division} = {divresult}")
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
