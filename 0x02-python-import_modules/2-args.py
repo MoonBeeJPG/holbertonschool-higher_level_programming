bbe7228e502a3f3b2e4ab8e4ce9fab020af29b49
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-04-28 08:57:21 -0700

    pycodestyle done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,14 +3,14 @@ import sys
 if __name__ == "__main__":
     arguments = len(sys.argv) - 1
     itinerate = 1
-    
+
     if arguments == 0:
         print(f"{arguments} arguments.")
     elif arguments == 1:
         print(f"{arguments} argument:")
     else:
         print(f"{arguments} arguments:")
-    
+
     while (arguments >= itinerate):
         print(f"{itinerate}: {sys.argv[itinerate]}")
         itinerate += 1
