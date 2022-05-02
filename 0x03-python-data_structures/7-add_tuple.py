0dee8903560145e8ac20230d242f22f7d1cbe432
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-02 13:43:30 -0700

    checkind task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a > 2 and tuple_b > 2:
-        my_tuple = tuple(map(lambda i, j: i + j, tuple_a[:2], tuple_b[:2]))
-        return my_tuple
-    elif tuple_a < 2 and tuple_b < 2:
-        result =
-        return my_tuple
-    else:
-         my_tuple = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
-        return my_tuple
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
