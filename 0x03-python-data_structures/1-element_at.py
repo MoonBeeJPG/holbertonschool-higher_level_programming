742daa9d893a30af6e5829918fe4df0d969b2e00
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-02 08:28:40 -0700

    None

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,3 +3,7 @@ def element_at(my_list, idx):
     if idx > 0:
         if idx <= len(my_list):
             return my_list[idx]
+        else:
+            return None
+    else:
+        return None
