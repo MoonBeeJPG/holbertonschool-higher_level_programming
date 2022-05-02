20838518df9ea097865483a24678a88f0ad08925
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-02 14:28:20 -0700

    checking task 12

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* is_palindrome - Write a function that checks if a sigly linked
+* list is a palindrome
+*
+* @head: header of the linked list 
+*
+* Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+int is_palindrome(listint_t **head)
+{
+	i = 0;
+	j = 0;
+	
+	if (!head)
+		return(1);
+	for d(i = 0; i < head; i++)
+
+}
