d890f5f873761db8e3321208c001e36b4be8d7cc
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-02 12:29:27 -0700

    check 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print(j, end=" ")
+        for j in item:
+            print(j, end=" " if j != item[-1] else "")
         print()
