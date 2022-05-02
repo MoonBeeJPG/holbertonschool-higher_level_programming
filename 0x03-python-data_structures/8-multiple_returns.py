961d746555db7002fd56ce0b5834cd2d38c234d2
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-02 12:26:20 -0700

    permissions

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return
-    else:
+    if sentence:
         for i in range(len(sentence)):
             a = sentence[0]
             b = len(sentence)
         return b, a
+    else:
+        return
