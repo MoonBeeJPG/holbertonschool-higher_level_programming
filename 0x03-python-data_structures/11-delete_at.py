ebb510dd4d7490a0927be1b6fc70f26e3b982095
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-02 13:55:09 -0700

    checkind task 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    else:
+        newe_list = list(my_list)
+        del newe_list[idx]
+        del my_list[idx]
+        return newe_list
+        return my_list
