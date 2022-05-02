35fbc158d6b41e735cae4c7af32b1aa1ca26f263
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-02 12:07:41 -0700

    checking task eight

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence:
+    if "null" in sentence:
+        return
+    elif:
         for i in range(len(sentence)):
             a = sentence[0]
             b = len(sentence)
