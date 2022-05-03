7a3161a7def69bda2876a649e6b39d1e544e6a1c
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-02 19:26:37 -0700

    trying again

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -13,10 +13,10 @@ int is_palindrome(listint_t **head)
 	unsigned int i, datos[2048], tam = 0;
 
 	if (head == NULL)
-		return(0);
+		return (0);
 	
 	if (*head == NULL)
-		return(1);
+		return (1);
 	
 	while (current != NULL)
 	{
@@ -24,8 +24,8 @@ int is_palindrome(listint_t **head)
 		tam += 1;
 	}
 	
-	if(tam == 1)
-		return(1);
+	if (tam == 1)
+		return (1);
 	
 	current = *head;
 	
@@ -38,7 +38,7 @@ int is_palindrome(listint_t **head)
 	for (i = 0; i <= (tam / 2); i++)
 	{
 		if (datos[i] != datos[tam - i - 1])
-			return(0);
+			return (0);
 	}
-	return(1);
+	return (1);
 }
