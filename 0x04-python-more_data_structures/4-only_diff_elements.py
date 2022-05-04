61d6a8d6cd2680fe0dee490514c38fe75b9ece94
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-04 09:25:52 -0700

    checkind again task four

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    first = set_1
-    second = set_2
-    if first ^ second:
-        return first ^ second
+    common = set(set_1) ^ set(set_2)
+    return common
