b1bb45105d92b3013d4c28dc048cd1a5a9781ab9
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-04 08:57:14 -0700

    task three done

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = set_1
-    elements = set_2
-    if (common & elements):
-        return common & elements
+    if set_1:
+        if set_2:
+            common = set_1
+            elements = set_2
+            if (common & elements):
+                return common & elements
