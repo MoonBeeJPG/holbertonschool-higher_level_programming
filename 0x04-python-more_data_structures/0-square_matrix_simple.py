65903ad6e57d68992a6bd1dfad0c32b6cfc585ed
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-04 08:32:38 -0700

    task two check

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = matrix
+    return new
+    return matrix
---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniques = set(list(my_list))
+    addition = sum(uniques)
+    return addition
