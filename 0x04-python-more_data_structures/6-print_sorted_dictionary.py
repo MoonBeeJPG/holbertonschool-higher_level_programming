6b363771892228d1eb8d6b273297e3ffe14e3c80
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-04 09:36:18 -0700

    why

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    listing = list(a_dictionary)
-    sort = sorted(listing)
-    return sort
+    return (list(sorted(a_dictionary)))
