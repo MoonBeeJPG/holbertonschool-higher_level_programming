cc02e00a4f3fc06c032904605d06fb561b63d272
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-04 14:10:21 -0700

    checking task twelve

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,36 @@
 #!/usr/bin/python3
+def convertor(value):
+    if value == 'I':
+        return 1
+    if value == 'V':
+        return 5
+    if value == 'X':
+        return 10
+    if value == 'L':
+        return 50
+    if value == 'C':
+        return 100
+    if value == 'D':
+        return 500
+    if value == 'M':
+        return 1000
+    return -1
 def roman_to_int(roman_string):
+    i = 0
+    result = 0
+    while (i < len(roman_string)):
+        current = convertor(roman_string[i])
+
+        if i + 1 < len(roman_string):
+            next_s = convertor(roman_string[i + 1])
+
+            if current >= next_s:
+                result += current
+                i += 1
+            else:
+                result += next_s - current
+                i += 2
+        else:
+            result += current
+            i += 1
+    return result
