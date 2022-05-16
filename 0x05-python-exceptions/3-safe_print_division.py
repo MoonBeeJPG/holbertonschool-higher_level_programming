0db6ff4071b455ec84a10411aac54a236032332f
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-16 16:55:30 -0700

    checkind task three

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    qtient = None
+    try:
+        qtient = a / b
+    except:
+        pass
+    finally:
+        if qtient is not None:
+            print("Inside result: {:.1f}".format(qtient))
+        else:
+            print("Inside result: None")
+    return qtient
