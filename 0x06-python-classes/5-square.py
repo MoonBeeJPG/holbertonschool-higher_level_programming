616ce87aee218076464d2c1191f28a76ea7a744c
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-17 16:44:26 -0700

    checking task five

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A class Square that defines a square based on 3-squared.py"""
+
+
+class Square:
+    """square class, empty"""
+    __size = None
+    """define the private __size attribute"""
+    def __init__(self, size=0):
+        """define the init for the square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """define and return the area of the square"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
