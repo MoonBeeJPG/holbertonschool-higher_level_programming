55b90d32c55131bd206dd4c864072af6f0b13469
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-17 15:02:56 -0700

    checking task one

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define class"""
+
+
+class Square:
+    __size = None
+    """Init the private size attribute"""
+    def __init__(self, size):
+        """Define the square"""
+        self.__size = size
