5488e8f047ab9f5ad18b009adb9f3284f62a4a13
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-17 18:20:13 -0700

    checking

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result  = 0
+    for i in range(a, b):
+        try:
+            if i > a
+                raise Exception('too far')
+            else
+                result += a ** b / c
+        except:
+            result = a + b
+            break
+    return result
