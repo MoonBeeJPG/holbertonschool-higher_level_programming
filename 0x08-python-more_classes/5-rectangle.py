70eb8140f79a5251d03c5fcf50de93381d492501
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-18 16:12:26 -0700

    checking task five

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""An class Rectangle that defines a rectangle based on 3-rectangle.py"""
+"""An class Rectangle that defines a rectangle based on 4-rectangle.py"""
 
 
 class Rectangle:
@@ -56,3 +56,6 @@ class Rectangle:
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
