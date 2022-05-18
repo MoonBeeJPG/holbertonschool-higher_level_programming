97c1776999a2d78bbd9d04aa9713bb816c267e9e
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-17 17:29:54 -0700

    checkind

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,9 +38,9 @@ class Square:
         """define position"""
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 posiive integers")
-        if not isinstance(value[0], int) or not isinstance(value[1], int:
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 posiive integers")
-        if value[0]) < 0 or value[1] < 0:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 posiive integers")
         else:
             self.__position = value
