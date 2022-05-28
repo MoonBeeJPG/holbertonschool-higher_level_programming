0f45fc988cfebdd8f5d8de16671a3e215895903f
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-27 17:20:22 -0700

    commit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,10 +7,10 @@ def text_indentation(text):
     """comment"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    chr1 = text.replace(". ", ".\n\n").replace(
+    text = text.replace(". ", ".\n\n").replace(
             "? ", "?\n\n").replace(": ", ":\n\n")
-    chr1 = text.split("\n")
+    text = text.split("\n")
 
-    for i in chr1:
+    for i in text:
         i = i.strip()
         print(i + "\n", end="")
