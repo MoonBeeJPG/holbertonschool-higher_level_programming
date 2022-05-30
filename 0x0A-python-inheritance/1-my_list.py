4b547fe3efe72612e5dfe87b88e2219a5207d358
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-30 16:04:26 -0700

    checking

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A class MyList that inherits from list """
+
+
+class MyList(list):
+    """ comment """
+    def print_sorted(self):
+        """ comment """
+        print(sorted(self))
