d8d55b5726b0f1213475282e7ad111d143a9fd13
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-30 18:18:55 -0700

    commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a class Rectangle that inherits from BaseGeometry """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle:
+    """ commit """
+    self.integer_validate("width", width)
+    self.integer_validate("height", height)
+    self.__width = width
+    self.__height = height
+
+    def area(self):
+        """ commit """
+        return self.__width * self.__height
+
+    def __str___(self):
+        """ commit """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
