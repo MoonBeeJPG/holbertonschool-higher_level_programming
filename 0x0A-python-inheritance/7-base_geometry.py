55b38be7a55e7793d41ab89d926f28d4d2130ae4
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-30 18:12:41 -0700

    commit

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" A class BaseGeometry based on 6-base_geometry.py """
+"""define thebase geometry class"""
 
 
 class BaseGeometry:
-    """ comment """
+    """baseclass for geometry objects"""
     def area(self):
-        """ comment """
+        """return area of geometry objects"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ comment """
-        if not isinstance(value, int):
+        """validate whether values is an integer"""
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
