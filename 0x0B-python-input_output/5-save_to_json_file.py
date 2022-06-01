3e574efbd0976622c48745eaa83d5f843b56423a
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-31 17:06:30 -0700

    checking pycodestyle again

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" a function that writes an object to a text file, using JSON 
+""" a function that writes an object to a text file, using JSON
     representation """
 import json
 
