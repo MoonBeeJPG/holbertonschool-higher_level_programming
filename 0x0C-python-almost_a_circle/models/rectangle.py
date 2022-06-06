4bef37e1b8b82b547c665cab384fda42567f6c1f
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-06-06 08:49:22 -0700

    commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,7 +84,7 @@ class Rectangle(Base):
     def __str__(self):
         """ return str """
         return "[Rectangle] ({}) {}/{} - {}/{}".\
-                format(self.id, self.x, self.y, self.width, self.height)
+            format(self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
         """ update arguments """
@@ -106,5 +106,5 @@ class Rectangle(Base):
     def to_dictionary(self):
         """ returns dictionary """
         dictionary = {'x': self.x, 'y': self.y, 'id': self.id,
-                'height': self.height, 'width': self.width}
+            'height': self.height, 'width': self.width}
         return dictionary
