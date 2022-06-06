8c2f44371b10bf69202f2c30a3e2fd00abaead69
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-06-06 16:24:36 -0700

    checking square again

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" a class Square that inherits from Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ creating class """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ init attributes """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ comment """
+        return size.width
+
+    @size.setter
+    def size(self, size):
+        """ setting size """
+        self.width = size
+        self.height = size
+
+    def update(self, *args, **kwargs):
+        """ updating arguments """
+        arguments = len(args)
+        if arguments > 0:
+            self.id = args[0]
+        if arguments > 1:
+            self.width = args[1]
+            self.height = args[1]
+        if arguments > 2:
+            self.x = args[2]
+        if arguments > 3:
+            self.y = args[3]
+        if arguments == 0:
+            for key, value in kwargs.items():
+                exec("self.{} = {}".format(key, value))
+
+    def to_dictionary(self):
+        dictionary = {"id": self.id, "size": self.width, "x": self.x,
+                      "y": self.y}
+        return dictionary
