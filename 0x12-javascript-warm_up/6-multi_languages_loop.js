9621da0e40e363b77a3590c0173a5b2fa71207ab
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-08-01 17:00:59 -0700

    Checking task six

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const Lines = ['C is Fun', 'Python is cool', 'JavaScripts is amazing'];
+const Lines = ['C is Fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < Lines.length; i++) {
   console.log(Lines[i]);
 }
