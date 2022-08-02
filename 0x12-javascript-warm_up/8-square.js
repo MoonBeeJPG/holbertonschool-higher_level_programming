aac14c5b8da5e39c8e759bc396884877dd3794d4
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-08-01 17:38:25 -0700

    Checking task eight

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myVar = process.argv;
+if (myVar[2]) {
+  for (let i = 0; i < parseInt(myVar[2]); i++) {
+    console.log('X'.repeat(myVar[2]));
+  }
+} else {
+  console.log('Missing size');
+}
