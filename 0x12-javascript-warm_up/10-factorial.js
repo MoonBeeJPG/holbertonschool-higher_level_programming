0f138e92c123ccc6afc5d409980bc88cd8a3efbc
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-08-01 18:25:15 -0700

    Checking task ten gain

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const myVar = process.argv;
 function factoralize (num) {
-  return (num * factoralize(num - 1));
+  if (num == 0) {
+    return 1;
+  } else {
+    return (num * factoralize(num - 1));
+  }
 }
 if (myVar[2]) {
   console.log(factoralize(parseInt(myVar[2])));
-} else {
+} else if (!myVar[2]) {
   console.log(1);
 }
