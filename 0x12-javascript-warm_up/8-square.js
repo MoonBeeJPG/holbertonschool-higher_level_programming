795f252858a06635a14a04c1e568751dc2707b1c
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-08-01 17:39:25 -0700

    Checking task eight again

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const myVar = process.argv;
-if (myVar[2]) {
+if (parseInt(myVar[2])) {
   for (let i = 0; i < parseInt(myVar[2]); i++) {
     console.log('X'.repeat(myVar[2]));
   }
