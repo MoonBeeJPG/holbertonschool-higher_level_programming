487cea7069e4cddcc449ff701ee445d70122a335
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-08-19 15:48:36 +0000

    Checking some tasks

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""  Python script that fetches https://intranet.hbtn.io/status """
+
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        body = response.read()
+        print("Body response:")
+        print("    - type: {}".format(type(body)))
+        print("    - content: {}".format(body))
+        print("    - utf8 content: {}".format(body.decode('utf8')))
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL and an email, sends a POST request to
+the passed URL with the email as a parameter, and displays the body of the response  """
+
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
+    email = {'email': argv[2]}
+    encoding = parse.urlencode(email)
+    data = encoding.encode('ascii')
+    with request.urlopen(argv[1], data) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
