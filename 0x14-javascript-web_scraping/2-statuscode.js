9a159136996f90d28b880c0418969dab8ff46e33
MoonBeeJPG <agustinasoledad@gmail.com>
2022-09-13 00:37:06 +0000

    Checking task two

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argument = process.argv
+const axios = require('axios').default;
+
+axios.get(argument[2]).then(function (response) {
+      console.log("code: " + response.status);
+    })
+    .catch(function (error) {
+      console.log("Code: " + error.response.status);
+});
