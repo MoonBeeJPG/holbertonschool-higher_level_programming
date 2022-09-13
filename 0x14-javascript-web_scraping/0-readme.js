b5a8eaf600f218e74c5a6d428e5bae1ad14efac7
MoonBeeJPG <agustinasoledad@gmail.com>
2022-09-13 00:08:44 +0000

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,9 @@ const argument = process.argv;
 const fs = require('fs');
 
 fs.readFile(argument[2], 'utf8', (error, response) => {
- if (error) {
-  console.error(error);
-  return;
- } else {
-  console.log(response);
- }
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(response);
+  }
 });
