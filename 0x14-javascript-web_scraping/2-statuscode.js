85bb823e6e6d9e7dd86eae25379008462b50c7a3
MoonBeeJPG <agustinasoledad@gmail.com>
2022-09-13 00:42:40 +0000

    semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const argument = process.argv
+const argument = process.argv;
 const axios = require('axios').default;
 
 axios.get(argument[2]).then(function (response) {
-    console.log("code: " + response.status);
+  console.log('code: ' + response.status);
 })
-    .catch(function (error) {
-      console.log("code: " + error.response.status);
-});
+  .catch(function (error) {
+    console.log('code: ' + error.response.status);
+  });
